bc6865e6dfe5b572c5ce2814ff037e5f56e2e23a
강동욱 <ehddnr8563@gmail.com>
2023-12-27 04:02:29 +0900

    add: videoDetail 라우팅 및 레이아웃 재사용

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { formatAgo } from '../util/date';
 
-export default function VideoCard({ video }) {
+export default function VideoCard({ video, type }) {
+  const navigate = useNavigate();
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const isList = type === 'list';
+  const handleClick = () => {
+    navigate(`/videos/watch/${video.id}`, { state: { video } });
+  };
 
   return (
-    <li>
-      <img className='w-full' src={thumbnails.medium.url} alt={title} />
+    <li className={`${isList ? 'flex gap-1 m-2 mt-0' : ''}`}>
+      <img
+        className={`rounded-md cursor-pointer ${
+          isList ? 'w-60 mr-2' : 'w-full'
+        }`}
+        src={thumbnails.medium.url}
+        alt={title}
+        onClick={handleClick}
+      />
       <div>
-        <p className='font-semibold my-2 line-clamp-2'>{title}</p>
+        <p
+          className='font-semibold my-2 mt-0 line-clamp-2 cursor-pointer  hover:underline underline-offset-2'
+          onClick={handleClick}
+        >
+          {title}
+        </p>
         <p className='text-sm opacity-80'>{channelTitle}</p>
         <p className='text-sm opacity-80'>{formatAgo(publishedAt, 'ko')}</p>
       </div>
